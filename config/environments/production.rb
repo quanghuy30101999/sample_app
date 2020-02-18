e210afdf764a3237df9f093492a9f4bd7d15c926
quanghuy <huytran.301099@gmail.com>
2020-02-18 10:24:21 +0700

    fix fog

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,7 +53,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://git.heroku.com/guarded-basin-72491.git'
+  host = 'https://quanghuycc.herokuapp.com/'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
