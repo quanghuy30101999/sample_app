1f9b7a49d48caae989921533f5237c3ef64e10d4
quanghuy <huytran.301099@gmail.com>
2020-02-19 10:52:58 +0700

    send grid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,10 +54,10 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   host = 'https://quanghuycc.herokuapp.com/'
-  config.action_mailer.default_url_options = { host: host }
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
-    :port           => '587',
+    :port           => 587,
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
